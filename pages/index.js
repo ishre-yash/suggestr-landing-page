da0b064ead474f57b8c77521fbf3659b0e5f39f9
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-07-08 18:43:17 +0530

    project setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Suggestr - Product Recommendations AI for Shopify</title>
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
